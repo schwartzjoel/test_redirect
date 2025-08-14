fb305afe33e984e5433551798205d2d901e05e95
schwartzjoel <schwartzjoel@gmail.com>
2025-08-14 13:21:28 -0400

    Initial commit (Vite app)

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs'; 
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem')
+    }
+  }
+});
